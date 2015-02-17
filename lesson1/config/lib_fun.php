5bb2ffe75cb3be8a6808088d5784d31b0029fdfc
vitaliy_eropkin <ist70@mail.ru>
2015-02-18 01:41:45 +0300

    написал по новому

--- lesson1/config/lib_fun.php
+++
@@ -1,5 +0,0 @@
-<?php
-Function mybd_sql($sql) {
-  return $result = mysql_query($sql);
-}
-?>
--- lesson1/moduls/select.php
+++
@@ -1,3 +0,0 @@
-<?php
-$sql = "SELECT * FROM news ORDER BY data_c DESC LIMIT 0,15";
-$result = mybd_sql($sql);
