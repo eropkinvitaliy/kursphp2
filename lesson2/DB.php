a09685b78070c1b937f3fb470f4fa67b073074b3
vitaliy_eropkin <ist70@mail.ru>
2015-02-22 19:12:29 +0300

    создал бд DB

--- lesson2/DB.php
+++ lesson2/DB.php
@@ -1,12 +1,11 @@
 <?php
 class DB
 {
-    public $bdname;
-
-    public function __construct()
+      public function __construct()
     {
         mysql_connect('localhost', 'root', '') or die('Нет коннекта');
         mysql_select_db('mynews') or die('Нет базы');
     }
 }
+
 ?>
