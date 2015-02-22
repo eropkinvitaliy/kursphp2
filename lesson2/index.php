e97c3683d2fe395ea11f43ca6aaea9f2f8d2a886
vitaliy_eropkin <ist70@mail.ru>
2015-02-22 19:53:14 +0300

    cоздал

--- lesson2/index.php
+++ lesson2/index.php
@@ -1,4 +1,8 @@
 <?php
-include __DIR__ . './moduls/DB.php';
-new db();
+include __DIR__ . '/moduls/DB.php';
+$news = new db();
+$sql = "SELECT * FROM news ORDER BY data_c DESC";
+if (!($news->execute($sql))) {
+    die ('Ошибка запроса');
+}
 ?>
