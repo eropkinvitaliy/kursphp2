bb20084e4dfea5d221b57cc586070cdd262f1eff
vitaliy_eropkin <ist70@mail.ru>
2015-02-22 21:26:39 +0300

    cоздал

--- lesson2/moduls/DB_NEWS.php
+++ lesson2/moduls/DB_NEWS.php
@@ -1,7 +1,28 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Vitaliy
- * Date: 22.02.2015
- * Time: 22:22
- */ 
+
+class DB_NEWS
+{
+
+    public function __construct()
+    {
+        mysql_connect('localhost', 'root', '') or die('Нет коннекта');
+        mysql_select_db('mynews') or die('Нет базы');
+    }
+
+    protected function query($sql) {
+        $res = mysql_query($sql);
+        $arr = [];
+        while (false !== $row = mysql_fetch_assoc($res)) {
+            $arr[] = $row;
+        }
+        return $arr;
+    }
+
+    public function News_getAll($table)
+    {
+        $sql = 'SELECT * FROM '.$table;
+        return $this->query($sql);
+    }
+}
+
+?>
