3198abc0e8ad29602278d8c8283372ac4ee5ca0d
vitaliy_eropkin <ist70@mail.ru>
2015-02-22 18:38:36 +0300

    cоздал

--- lesson2/moduls/bd_news.php
+++ lesson2/moduls/bd_news.php
@@ -9,15 +9,8 @@
  Перепишите предыдущее задание, используя
 классы новостей и базы данных*/
 
-abstract class Artical
 
-{
-
-
-    abstract public function __construct($bdname);
-}
-
-class Database_work extends Artical
+class Database_work
 {
     public $bdname;
     public $tabname;
@@ -27,68 +20,11 @@ class Database_work extends Artical
     public $temp;
     private $row;
 
-    public  function __construct($bdname)
+    public function __construct($bdname)
     {
         $this->bdname = $bdname;
         mysql_connect('localhost', 'root', '') or die('Нет коннекта');
         mysql_select_db($bdname) or die('Нет базы');
     }
-
-    public function viewbase()
-    {
-        $this->sql = "SELECT * FROM news ORDER BY data_c DESC ";
-        return $this->Sql_qwery(sql);
-    }
-
-    public function view_full_base($id_n)
-    {
-        $this->sql = "SELECT * FROM news WHERE id = '$id_n'";
-        return $this->Sql_qwery(sql);
-    }
-
-    public function Sql_qwery($sql)
-    {
-        $this->res = mysql_query($this->sql);
-        $this->temp = [];
-        while (false !== $this->row = mysql_fetch_assoc($this->res)) {
-            $this->temp[] = $this->row;
-        }
-        return $this->temp;
-    }
-
-    public function News_getString($id_n)
-    {
-        $this->sql = "SELECT * FROM news WHERE id = '$id_n'";
-        return Sql_query(sql);
-    }
-
-    public function News_add($ntitle, $ntext, $nuser, $now)
-    {
-        $this->sql = "INSERT INTO news(title,text_f,data_c,user_n) VALUES ('$ntitle','$ntext','$now','$nuser')";
-        if (mysql_query($this->sql)) {
-            return true;
-        } else {
-            return die ('Ошибка. Запись не добавлена');
-        }
-    }
 }
-
-function Is_Get()
-{
-    if (isset($_GET['id'])) {
-        return true;
-    } else {
-        return die('ошибка открытия текста новости');
-    }
-}
-
-function Is_Post()
-{
-    if (isset($_POST['title']) && isset($_POST['users']) && isset($_POST['newstext'])) {
-        return true;
-    } else {
-        return die('ошибка отправлены пустые данные');
-    }
-}
-
 ?>
