373f2d62834127cdd5474348385fc96328941778
vitaliy_eropkin <ist70@mail.ru>
2015-02-22 21:29:38 +0300

    исправил индекс

--- lesson2/index.php
+++ lesson2/index.php
@@ -1,7 +1,9 @@
 <?php
 include __DIR__ . '/moduls/DB.php';
+include __DIR__ . '/moduls/DB_NEWS.php';
 $db = new DB();
 $table = 'news';
-$news = $db->News_getAll($table);
+$db_news = new DB_NEWS();
+$news = $db_news->News_getAll($table);
 var_dump($news);
 ?>
