b4b269877145e769f6c0baeed81c75fb9d1edf4f
vitaliy_eropkin <ist70@mail.ru>
2015-02-22 19:23:43 +0300

    cоздал

--- lesson2/DB.php
+++ lesson2/DB.php
@@ -1,11 +1,20 @@
 <?php
+
 class DB
 {
-      public function __construct()
+    public function __construct()
     {
         mysql_connect('localhost', 'root', '') or die('Нет коннекта');
         mysql_select_db('mynews') or die('Нет базы');
     }
+    public function execute ($sql) {
+        
+    }
 }
 
 ?>
+
+напишите мне публичный метод этого класса
+назовите его execute($sql)
+он должен выполнить запрос к базе, находящийся в $sql
+и вернуть признак успешности запроса - true или false
