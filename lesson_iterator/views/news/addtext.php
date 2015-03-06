054bd4dc62a036d88df0c3c07a4f7b4eddd9ff19
vitaliy_eropkin <ist70@mail.ru>
2015-03-06 16:00:16 +0300

    чистка файлов

--- lesson_iterator/views/news/addtext.php
+++ lesson_iterator/views/news/addtext.php
@@ -1,16 +1,17 @@
+<!-- Форма ввода новой новости  -->
 <html>
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     <link href="./css/style.css" rel="stylesheet" type="text/css"/>
-    <title>Редактирование новости</title>
+    <title>Добавление новостей</title>
 </head>
 <body>
-<h3>Редактирование новости</h3>
+<h3>Добавление новостей</h3>
 
-<form action="./index.php?ctrl=Admin&act=Add" method="POST">
+<form action="../lesson3/index.php?ctrl=Admin&act=Add" method="POST">
     Заголовок новости: <input type="text" Size=70 name="title" required=""><br>
-    Ваше имя: <input type="text" Size=50 name="user" required=""><br>
-    Текст <textarea name="text" cols=60 rows=6 required=""> </textarea>
+    Ваше имя: <input type="text" Size=50 name="users" required=""><br>
+    Текст <textarea name="newstext" cols=60 rows=6 required=""> </textarea>
     <br><br>
     <input type="Submit" Value="Ok">
     <input type="reset" Value=" Очистить ">
