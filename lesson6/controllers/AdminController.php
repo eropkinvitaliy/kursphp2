4906bea4c9028a066482fd3439a03d0d458a5eb7
vitaliy_eropkin <ist70@mail.ru>
2015-03-07 14:43:08 +0300

    не много коментариев добавил

--- lesson6/controllers/AdminController.php
+++ lesson6/controllers/AdminController.php
@@ -25,22 +25,22 @@ class AdminController
 
     public function actionUpdate()
     {
-        session_start();
+        session_start(); // запускаем сессию для контроля id новости, которую редактируем
         $id = $_GET['id'];
         if (!isset($id)) {
-            $id = $_SESSION['id'];
+            $id = $_SESSION['id']; //берём id из сессии, после апдейта, для показа новости которую проапдейтили
         } else {
-            $_SESSION['id'] = $id;
+            $_SESSION['id'] = $id; //записываем id в сессию, что бы после апдейта сохранить эту новость
         }
         $view = new NewsModel();
         $view->title = isset($_POST['title']) ? $_POST['title'] : null;
         $view->text = isset($_POST['text']) ? $_POST['text'] : null;
         $view->id = $id;
-        if (empty($view->title)) {
+        if (empty($view->title)) {    //если заголовка пока нет, ищем по id новость и выводим на экран форму редактора
             $view = NewsModel::findOneByPk($id);
             include __DIR__ . '/../views/news/update.php';
         }
-        else {
+        else { //заголовок новости пришёл из формы редактора и мы обновляем новость и переходим к Фронтконтроллеру
             $view->save();
             header('Location: http://localhost/php-2/lesson6/index.php');
         }
