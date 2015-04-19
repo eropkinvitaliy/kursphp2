58fd4849fdd94ed927ebf3999cb3346f8138b6ce
vitaliy_eropkin <ist70@mail.ru>
2015-04-19 20:20:17 +0300

    Начало КДМ-21

---
+++ test/Myclass.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Vitaliy
+ * Date: 28.02.2015
+ * Time: 20:13
+ */
+
+class Myclass {
+
+} 
---
+++ test/index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Vitaliy
+ * Date: 28.02.2015
+ * Time: 20:13
+ */ 
