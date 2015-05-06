c32e85481df5d0874de2a7f75624b43bd14c9122
vitaliy_eropkin <ist70@mail.ru>
2015-05-06 22:57:32 +0300

    В конструкторе загружает в память изображение, чей путь ему передан. Иимеет методы типа filter*(), которые что-то с изображением делают Имеет метод save(), который сохранит готовое изображение

--- KDM-21/protected/Components/ImageProcessor.php
+++ KDM-21/protected/Components/ImageProcessor.php
@@ -5,67 +5,68 @@ namespace App\Components;
 class ImageProcessor
 {
     public $image;
-    public $newimage;
-    public $pathToImage;
-    public $widthLoadImage;
-    public $heightLoadImage;
+    public $path;
 
-    public function __construct($pathToImage)
+    public function __construct($path)
     {
-        $this->pathToImage = \T4\Fs\Helpers::getRealPath($pathToImage);
-        $imageinfo = getimagesize($this->pathToImage);
+        $this->pathToImage = \T4\Fs\Helpers::getRealPath($path);
+        $imageinfo = getimagesize($this->path);
         $imagetype = $imageinfo[2];
         if ($imagetype == IMAGETYPE_JPEG) {
-            $this->image = imagecreatefromjpeg($this->pathToImage);
+            $this->image = imagecreatefromjpeg($this->path);
         } elseif ($imagetype == IMAGETYPE_GIF) {
-            $this->image = imagecreatefromgif($this->pathToImage);
+            $this->image = imagecreatefromgif($this->path);
         } elseif ($imagetype == IMAGETYPE_PNG) {
-            $this->image = imagecreatefrompng($this->pathToImage);
+            $this->image = imagecreatefrompng($this->path);
         }
-        $this->widthLoadImage = imagesx($this->image);
-        $this->heightLoadImage = imagesy($this->image);
     }
 
     public function filterResize($width, $height)
     {
-        $this->newimage = imagecreatetruecolor($width, $height);
-        imagecopyresampled($this->newimage, $this->image, 0, 0, 0, 0, $width, $height,
-            $this->widthLoadImage, $this->heightLoadImage);
+        $newimage = imagecreatetruecolor($width, $height);
+        imagecopyresampled($newimage, $this->image, 0, 0, 0, 0, $width, $height,
+            $this->getWidth(), $this->getHeight());
+        $this->image = $newimage;
+        return $this;
     }
 
     public function filterResizeToHeight($height)
     {
-        $ratio = $height / $this->heightLoadImage;
-        $width = $this->widthLoadImage * $ratio;
+        $ratio = $height / $this->getHeight();
+        $width = $this->getWidth() * $ratio;
         $this->filterResize($width,$height);
+        return $this;
     }
     public function filterResizeToWidth($width)
     {
-        $ratio = $width / $this->widthLoadImage;
-        $height = $this->heightLoadImage * $ratio;
+        $ratio = $width / $this->getWidth();
+        $height = $this->getHeight() * $ratio;
         $this->filterResize($width,$height);
+        return $this;
     }
     public function filterZoom($zoom)
     {
-        $width = $this->widthLoadImage * $zoom/100;
-        $height = $this->heightLoadImage * $zoom/100;
+        $width = $this->getWidth() * $zoom/100;
+        $height = $this->getHeight() * $zoom/100;
         $this->filterResize($width,$height);
+        return $this;
     }
 
      public function save($imagetype = IMAGETYPE_JPEG, $compression = 75, $permissions = null)
     {
         if ($imagetype == IMAGETYPE_JPEG) {
-            imagejpeg($this->newimage, $this->pathToImage, $compression);
+            imagejpeg($this->image, $this->path, $compression);
         } elseif ($imagetype == IMAGETYPE_GIF) {
-            imagegif($this->newimage, $this->pathToImage);
+            imagegif($this->image, $this->path);
         } elseif ($imagetype == IMAGETYPE_PNG) {
-            imagepng($this->newimage, $this->pathToImage);
+            imagepng($this->image, $this->path);
         }
         if ($permissions != null) {
-            chmod($this->pathToImage, $permissions);
+            chmod($this->path, $permissions);
         }
+        return $this;
     }
- /*   public function getWidth()
+    public function getWidth()
     {
         return imagesx($this->image);
     }
@@ -73,6 +74,6 @@ class ImageProcessor
     public function getHeight()
     {
         return imagesy($this->image);
-    }*/
+    }
 
 } 
